77658fbf2ee67f9be26c2001483b330b1aca8672
Girija Kandel <71377128+girikandel@users.noreply.github.com>
2025-07-20 22:58:19 -0400

    Update API endpoints for create user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,7 +3,7 @@ import userCtrl from '../controllers/user.controller.js'
 
 const router = express.Router()
 
-router.post('/', userCtrl.create)
+router.post('/create', userCtrl.create)
 router.get('/', userCtrl.list)
 router.put('/:userId', userCtrl.update)
 router.delete('/:userId', userCtrl.remove)
