d78bedbd2e4262ab1249fef7c8f451c80cbb2857
Joshua Desroches <josh.des.21@gmail.com>
2025-08-01 01:13:14 -0400

    Fix React 18 compatibility: downgrade framer-motion to 10.18.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App-simple.jsx";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
